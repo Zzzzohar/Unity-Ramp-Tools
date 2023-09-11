a8348071ca60f91474c4ad9e0cde357d5f245e1f
zhouhao <2422538491@qq.com>
2023-09-11 20:11:54 +0800

    优化一下功能

--- GradientCreator.cs
+++ GradientCreator.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
 using System.IO;
+
 public class GradientCreator : EditorWindow
 {
-    [MenuItem("Tools/AssetsEditor/GradientCreator")]
+    [MenuItem("*Athena*/ArtistTools/GradientCreator")]
     private static void ShowWindow()
     {
         var window = GetWindow<GradientCreator>();
@@ -13,111 +14,139 @@ public class GradientCreator : EditorWindow
     }
 
     ///<绘制面板>
-    private int _GradientWidth = 128;//每一条渐变的宽度
-    private int _GradientHeight = 4;//每一条渐变的高度
+    private GradientCreatorData.WidthSize _GradientWidth = GradientCreatorData.WidthSize._128;//每一条渐变的宽度
+    private int _GradientHeight = 2;//每一条渐变的高度
     public GradientCreatorData _GradientData;
     private Texture2D _GradientMap;
+    string _GradientName = "Modify here";
+    public GradientCreatorData.Format index;
+    int FormatIndex = 0;
+    private string _Format = ".tga";
+    
     private void OnGUI()
     {
+        EditorGUI.BeginChangeCheck();
         _GradientData = EditorGUILayout.ObjectField("GradientData", _GradientData, typeof(GradientCreatorData), false) as GradientCreatorData;
-        if (_GradientData)
+        if (EditorGUI.EndChangeCheck())//结束检查是否有修改
         {
-            _GradientWidth = _GradientData._GradientWidth;
-        }
-        else
-        {
-            _GradientWidth = 128;
+            AnalyzeData(_GradientData);
         }
 
 
         GradientListGUI();
 
-        _GradientMap = Create(_Gradient, _GradientWidth, _Gradient.Count * _GradientHeight);
-        _GradientMap.wrapMode = TextureWrapMode.Clamp;
-        //无需保存贴图也能传递给shader
-        Shader.SetGlobalTexture("_Gradient", _GradientMap);
+        _GradientMap = Create(_Gradient, (int)_GradientWidth, _Gradient.Count * _GradientHeight);
         SceneView.RepaintAll();
-        Save();
+        DrawGUI();
+        SaveTextureAndSaveData();
     }
+
+    public void AnalyzeData(GradientCreatorData data)
+    {
+        if (!data) return;
+        _GradientWidth = data._GradientWidth;
+        _GradientName = data._GradientName;
+        _Gradient = data._Gradient;
+        _Gradient = _GradientData._Gradient;
+    }
+    
 
     ///<绘制渐变控制列表>
-    [SerializeField]//必须要加
-    protected List<Gradient> _Gradient = new List<Gradient>();
-    protected SerializedObject _serializedObject;    //序列化对象
-    protected SerializedProperty _assetLstProperty;   //序列化属性
+    [SerializeField]
+    public List<Gradient> _Gradient = new List<Gradient>();
+    protected SerializedObject _serializedObject;    
+    protected SerializedProperty _assetLstProperty;  
     private void GradientListGUI()//绘制列表
     {
-        
-        if (_GradientData)
+        if (_Gradient.Count <= 0)
         {
-            _Gradient = _GradientData._Gradient;
+            Gradient temp = new Gradient();
+            _Gradient.Add(temp);
         }
-        
-        
         _serializedObject.Update();
-        //开始检查是否有修改
-        EditorGUI.BeginChangeCheck();
         EditorGUILayout.PropertyField(_assetLstProperty, true);//显示属性 //第二个参数必须为true，否则无法显示子节点即List内容
-        if (EditorGUI.EndChangeCheck())//结束检查是否有修改
-        {
-            _serializedObject.ApplyModifiedProperties();//提交修改
-        }
-    }
-    ///<存储纹理>
-    string _GradientName;
 
-    string[] MapFormat = {  "TGA", "PNG","JPG" };
-    int FormatIndex = 0;
-    private void Save()
+    }
+    //绘制GUI
+    private void DrawGUI()
     {
+        _GradientWidth = (GradientCreatorData.WidthSize)EditorGUILayout.EnumPopup("每条渐变宽度(像素)",_GradientWidth, GUILayout.Width(800));
         
-        if (_GradientData)
-        {
-            _GradientData._GradientWidth = EditorGUILayout.IntField("每条渐变宽度(像素)", _GradientWidth);
-            _GradientHeight = _GradientData._GradientHeight;
-            _GradientData._GradientHeight = EditorGUILayout.IntField("每条渐变高度(像素)", _GradientHeight);
-        }
-        else
-        {
-            _GradientWidth = EditorGUILayout.IntField("每条渐变宽度(像素)", _GradientWidth);
-            _GradientHeight = EditorGUILayout.IntField("每条渐变高度(像素)", _GradientHeight);
-        }
         EditorGUILayout.BeginHorizontal();
         EditorGUILayout.LabelField("纹理名称", GUILayout.Width(100));
-        if (_GradientData)
+
+        _GradientName = EditorGUILayout.TextArea(_GradientName);
+
+        index = (GradientCreatorData.Format)EditorGUILayout.EnumPopup(index, GUILayout.Width(100));
+        _Format = ".tga";
+        if (index == GradientCreatorData.Format.TGA)
         {
-            _GradientName = _GradientData._GradientName;
-            _GradientData._GradientName = EditorGUILayout.TextArea(_GradientName);
-        }
-        else
+            _Format = ".tga";
+        }else if (index == GradientCreatorData.Format.PNG)
         {
-            _GradientName = EditorGUILayout.TextArea(_GradientName);
+            _Format = ".tga";
+        }else
+            _Format = ".jpg";
+        EditorGUILayout.EndHorizontal();
+    }
+    ///<存储纹理>
+    private void SaveTextureAndSaveData()
+    {
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Save Texture"))
+        {
+            string path = EditorUtility.SaveFolderPanel("Select an output path", _GradientData?_GradientData.path : "", "");
+            string subPath = path.Substring(Application.dataPath.Length - 6);
+            // Save the texture to disk
+            byte[] pngData = _GradientMap.EncodeToTGA();
+            if (index == GradientCreatorData.Format.TGA)
+                pngData = _GradientMap.EncodeToTGA();
+            if (index == GradientCreatorData.Format.PNG)
+                pngData = _GradientMap.EncodeToPNG();
+            if (FormatIndex == 2)
+                pngData = _GradientMap.EncodeToJPG();
+
+            
+            File.WriteAllBytes(path + "/" + _GradientName + _Format, pngData);
+            AssetDatabase.Refresh();
+            
+            TextureImporter importer = (TextureImporter)TextureImporter.GetAtPath(subPath + "/" + _GradientName + _Format);
+            TextureImporterSettings setting = new TextureImporterSettings();
+            if (importer != null)
+            {
+                importer.ReadTextureSettings(setting);
+                setting.wrapMode = TextureWrapMode.Clamp;
+                setting.mipmapEnabled = false;
+                importer.SetTextureSettings(setting);
+                importer.textureCompression = TextureImporterCompression.Uncompressed;
+                importer.SaveAndReimport();
+            }
+
+            if (_GradientData && path == _GradientData.path)
+            {
+                _GradientData._Gradient = _Gradient;
+                _GradientData._GradientName = _GradientName;
+                _GradientData._GradientWidth = _GradientWidth;
+                _GradientData.format = index;
+                _GradientData.path = path;
+            }
+            else
+            {
+                GradientCreatorData data = ScriptableObject.CreateInstance<GradientCreatorData>();
+                data._Gradient = _Gradient;
+                data._GradientName = _GradientName;
+                data._GradientWidth = _GradientWidth;
+                data.format = index;
+                data.path = path;
+                AssetDatabase.CreateAsset(data, subPath + "/" + _GradientName + ".asset");
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+                _GradientData = data;
+            }
             
         }
         
-        FormatIndex = EditorGUILayout.Popup(FormatIndex, MapFormat,GUILayout.Width(100));
-        string _Format = ".tga";
-        if (FormatIndex == 0)
-            _Format = ".tga";
-        if (FormatIndex == 1)
-            _Format = ".tga";
-        if (FormatIndex == 2)
-            _Format = ".jpg";
         EditorGUILayout.EndHorizontal();
-
-        if (GUILayout.Button("Save"))
-        {
-            string path = EditorUtility.SaveFolderPanel("Select an output path", "", "");
-
-            if (_GradientData)
-            {
-                _GradientData._Gradient = this._Gradient;
-            }
-
-            byte[] pngData = _GradientMap.EncodeToPNG();
-            File.WriteAllBytes(path + "/" + _GradientName + _Format, pngData);
-            AssetDatabase.Refresh();
-        }
     }
 
     ///<序列化属性列表>
--- GradientCreatorData.cs
+++ GradientCreatorData.cs
@@ -4,9 +4,27 @@ using System.Collections.Generic;
 [CreateAssetMenu(fileName = "GradientCreatorData", menuName = "Data/GradientCreatorData", order = 0)]
 public class GradientCreatorData : ScriptableObject
 {
+    [HideInInspector]
+    public string path = " ";
     public string _GradientName = "Gradient";
-    public int _GradientWidth = 128;//每一条渐变的宽度
-    public int _GradientHeight = 4;//每一条渐变的高度
+    public enum WidthSize
+    {
+        _64 = 64,
+        _128 = 128,
+        _256 = 256,
+        _512 = 512,
+        _1024 = 1024,
+        _2048 = 2048,
+    }
+
+    public WidthSize _GradientWidth = WidthSize._128;//每一条渐变的宽度
+    private int _GradientHeight = 2;//每一条渐变的高度
     
     public List<Gradient> _Gradient = new List<Gradient>();
+    public enum Format
+    {
+        TGA, PNG,JPG
+    }
+
+    public Format format;
 }
